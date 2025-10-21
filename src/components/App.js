eb808d38ce82eb2c74cf5626e4499adda2baf3d3
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-10-22 01:10:12 +0530

    Refactor App component and update markup3

--- src/components/App.js
+++ src/components/App.js
@@ -5,51 +5,46 @@ import './../styles/App.css';
 import React, { useState } from "react";
 
 
-const App = () => {
+export default function App() {
   const [bill, setBill] = useState(50);
   const [tipPercent, setTipPercent] = useState(18);
   const [people, setPeople] = useState(1);
 
-  const totalTip = bill * (tipPercent / 100);
-  const tipPerPerson = (totalTip / people).toFixed(2);
+  const tipPerPerson = ((bill * (tipPercent / 100)) / people).toFixed(2);
 
   return (
     <div id="container">
-      <h2>Tip Calculator</h2>
+      <h1>Tip Calculator</h1>
 
-      <div>
-        <label htmlFor="billamt">Bill:</label>
-        <input
-          id="billamt"
-          type="number"
-          value={bill}
-          onChange={(e) => setBill(Number(e.target.value))}
-        />
-      </div>
+      <label htmlFor="billAmt">Bill:</label>
+      <input
+        id="billAmt"
+        type="number"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
-      <div>
-        <label htmlFor="tipamt">Tip Percentage:</label>
-        <input
-          id="tipamt"
-          type="number"
-          value={tipPercent}
-          onChange={(e) => setTipPercent(Number(e.target.value))}
-        />
-      </div>
+      <br />
 
-      <div>
-        <label htmlFor="peopleamt">Number of People:</label>
-        <input
-          id="peopleamt"
-          type="number"
-          value={people}
-          onChange={(e) => setPeople(Number(e.target.value))}
-        />
-      </div>
+      <label htmlFor="serviceQual">Tip Percentage:</label>
+      <input
+        id="serviceQual"
+        type="number"
+        value={tipPercent}
+        onChange={(e) => setTipPercent(Number(e.target.value))}
+      />
 
-      <p>Tip Per Person: ${tipPerPerson}</p>
+      <br />
+
+      <label htmlFor="peopleAmt">Number of People:</label>
+      <input
+        id="peopleAmt"
+        type="number"
+        value={people}
+        onChange={(e) => setPeople(Number(e.target.value))}
+      />
+
+      <p id="tipPerPerson">Tip Per Person: ${tipPerPerson}</p>
     </div>
   );
-};
-
-export default App;
+}
